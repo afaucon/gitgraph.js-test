58cafe32c6fbba64c01cf8bc35f94d66ba272d60
Adrien Faucon <adrien.faucon@gmail.com>
2021-07-10 07:51:58 +0200

    New branching example

--- src/components/GitGraph.js
+++ src/components/GitGraph.js
@@ -9,18 +9,41 @@ function GitGraph() {
         master.commit("Initial commit");
 
         const develop = master.branch("develop");
-        develop.commit("Add TypeScript");
+        develop.commit("Initiate new development");
 
-        const aFeature = develop.branch("a-feature");
-        aFeature
+        const feature01 = develop.branch("feature#01");
+        feature01
           .commit("Make it work")
           .commit("Make it right")
           .commit("Make it fast");
+        develop.merge(feature01);
+        // feature01.delete()
 
-        develop.merge(aFeature);
-        develop.commit("Prepare v1");
+        const feature02 = develop.branch("feature#02");
+        feature02
+          .commit("Make it work")
+          .commit("Make it right")
+          .commit("Make it fast");
+        develop.merge(feature02);
+        // feature02.delete()
 
-        master.merge(develop).tag("v1.0.0");
+        const release = develop.branch("Release");
+        // release.commit("Prepare next version");
+
+        release.merge(develop)
+        release
+          .commit("Release v1");
+
+        const defect01 = release.branch("defect#01");
+        defect01
+          .commit("Defect correction");
+        release.merge(defect01);
+        // defect01.delete()
+
+        develop.merge(release);
+        master.merge(release).tag("v1.0.0");;
+        // release.delete()
+
       }}
     </Gitgraph>
   );
